bb5411275569a31e0d57889e2b19f778bada9b9e
Kasongo Mukebo Ben <l.benkasmukebo7@gmail.com>
2023-06-13 01:03:04 +0200

    Update kata_1.js
    Update my answer

--- kata_1.js
+++ kata_1.js
@@ -3,29 +3,27 @@
 
 function findOddNames(list) {
   // thank you for checking out the Coding Meetup kata :)
-  // 1 => Loop throuth the array of oject and retrive the firstName 
-  // 2 => calculated the ASCII firstNAme leeter 
-  // 3 => Check if the sum is an odd and return it otherwise return an empty array []
+  // 1 => Loop through the array of objects and retrieve the firstName 
+  // 2 => calculated the ASCII first name letter 
+  // 3 => Check if the sum is odd and return it otherwise return an empty array []
   let findEl = [];
-  let firstName = list.map(el => {
-    let firstEl = el.firstName;
-    let sum = 0;
 
-    for(let i = 0; i < firstEl.length; i++){
-      let code = firstEl.charCodeAt(i);
-      sum = sum + code;
-    }
+   let firstName = list.map(el => {
+     let firstEl = el.firstName;
+     let sum = 0;
 
-    if(sum % 2 != 0) {
-      findEl.push(el);
-      return findEl;
-    }
-    else {
-      if(findEl.length == list.length) {
-        return findEl = [];
-      }
-    } 
+     for(let i = 0; i < firstEl.length; i++){
+       let code = firstEl.charCodeAt(i);
+       sum += code;
+     }
+
+     if(sum % 2 != 0) {
+       findEl.push(el);
+       return findEl;
+     }
    });
 
    return findEl;
 }
+
+// return d.firstName.split('').reduce(((r, e) => r + e.charCodeAt(0)), 0) % 2 !== 0 // Best, only online 
