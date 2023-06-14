66f52e685749986bf25b5c27dfb6f4479ee68200
Kasongo Mukebo Ben <l.benkasmukebo7@gmail.com>
2023-06-15 00:36:11 +0200

    Update kata_2.js
    Add first solution

--- kata_2.js
+++ kata_2.js
@@ -1,2 +1,19 @@
 // https://www.codewars.com/kata/5a2950621f7f70c12d000073/train/javascript
 // Compression : impossible
+
+const mangle = (integers) => {
+   // 1 => Pick a random number from the array.
+   //  get random index value
+    const randomIndex = Math.floor(Math.random() * integers.length);
+   //  get a random item
+   //  const item = integers[randomIndex];
+    const item = integers.splice(randomIndex, 1);
+    return  integers;
+};
+
+const guess = (mangled) => {
+  // 2 => Return the remaining number.
+    console.log(mangled);
+    return mangled;
+    // return mangled[0];
+}
